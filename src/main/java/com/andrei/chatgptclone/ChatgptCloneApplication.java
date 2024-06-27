d25ffc38098c349e6eaf3f7ffcb4ae2769c61e0e
Andrei <andreibalzan@gmail.com>
2024-06-27 08:30:45 +0100

    initial commit

---
+++ src/main/java/com/andrei/chatgptclone/ChatgptCloneApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.chatgptclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrei/chatgptclone/ChatgptCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.chatgptclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
